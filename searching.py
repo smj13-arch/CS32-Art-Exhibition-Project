258f59cef0313042722146d2ea7ee9d56b895dcd
AmelieryChen <ameliechen1026@gmail.com>
2026-04-23 19:59:09 +0000

    Thu, Apr 23, 2026, 3:59 PM -04:00

--- searching.py
+++ searching.py
@@ -6,3 +6,9 @@ def searching_function(start_time, end_time, artist, country, material):
     base_url = "https://api.harvardartmuseums.org/object"
     api_key = "d085cac8-e2aa-425f-b3d5-2c5b49d15fc0"
 
+    params = {
+        "apikey": api_key,
+        "size": 10,
+        "q": f"datebegin:[{start_time} TO {end_time}] AND people.name:{artist} AND culture:{country} AND medium:{material}"
+    }   
+

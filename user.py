a42838e1248c48659932178170d3a8e1a204ea2f
smj13-arch <smjoeveer@gmail.com>
2026-05-03 12:24:58 -0400

    made a cleaner intro message

---
+++ user.py
@@ -0,0 +1,16 @@
+# Instructions and welcome user to ART EXHIBITOR
+
+intro_text = """
+### Welcome to ART EXHIBITOR: Dream Gallery ###
+
+We are excited to provide you a tool to help visualize your dream art gallery,
+using pieces from our beautiful Harvard Art Museums! Either provide specific search
+parameters to gather the pieces you've been dreaming of, but couldn't quite name—
+or leave it up to us and generate a random virtual gallery.
+
+We suggest gallery sizes of between 5–10 pieces; the maximum capacity is 10.
+Happy creating!
+"""
+
+print(intro_text) # utilizing multiline print statement
+

a4b79408c6d003eef3d8c809bdfffd37066543f9
AmelieryChen <ameliechen1026@gmail.com>
2026-05-03 20:30:37 +0000

    Sun, May 3, 2026, 4:30 PM -04:00

--- user.py
+++ user.py
@@ -4,12 +4,12 @@ intro_text = """
 ### Welcome to ART EXHIBITOR: Dream Gallery ###
 
 We are excited to provide you a tool to help visualize your dream art gallery,
-using pieces from our beautiful Harvard Art Museums! Either provide specific search
-parameters to gather the pieces you've been dreaming of, but couldn't quite name—
+using pieces from our very own Harvard Art Museums! To get started, either provide
+specific search parameters to gather the pieces you've been dreaming of, but couldn't quite name--
 or leave it up to us and generate a random virtual gallery.
 
 We have the capacity to create virtual galleries of 5 pieces, including short title
-cards and descriptions beneath each piece. Happy creating!
+cards and descriptions beneath each piece. Happy curating! :D
 """
 
 print(intro_text) # utilizing multiline print statement
